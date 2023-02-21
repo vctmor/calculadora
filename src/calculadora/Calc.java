b68c535dfebd793f8bb3bac0761b8168eddf1189
vctmor <125832442+vctmor@users.noreply.github.com>
2023-02-21 18:25:52 -0300

    Add files via upload
    primeiro commit

---
+++ src/calculadora/Calc.java
@@ -0,0 +1,13 @@
+package calculadora;
+import java.util.Scanner
+
+public class Calc {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author faunu
+ *
+ */
+module calculadora {
+}
